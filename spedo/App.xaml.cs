1a09dd3c15bde6503570b1b665053b3b8edf6a20
Naoki <max_fabian@hotmail.com.ar>
2025-06-26 01:44:49 -0300

    First commit

---
+++ spedo/App.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.System.Profile;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace spedo
+{
+
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public sealed partial class App : Application
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="App"/> class.
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            //Suspending += OnSuspending;
+            InitializeComponent();
+        }
+
+        protected override async void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            await RunAppInitialization(null);
+        }
+
+        private async System.Threading.Tasks.Task RunAppInitialization(string launchParameters)
+        {
+
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            if (rootFrame == null)
+            {
+                // Crear un nuevo Frame si no existe
+                rootFrame = new Frame();
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                // Navegar a la MainPage
+                rootFrame.Navigate(typeof(MainPage1), launchParameters);
+            }
+
+            // Mostrar la ventana
+            Window.Current.Activate();
+        }
+
+
+        /// <summary>
+        /// Invoked when Navigation to a certain page fails
+        /// </summary>
+        /// <param name="sender">The Frame which failed navigation</param>
+        /// <param name="e">Details about the navigation failure</param>
+        private void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+    }
+}
---
+++ spedo/MainPage1.xaml.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Windows.Devices.Geolocation;
+using Windows.System.Display;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.Devices.Power;
+using Windows.System.Power;
+using Windows.Graphics.Display;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Media;
+using Windows.UI;
+
+namespace spedo
+{
+    public partial class MainPage1 : Page
+    {
+        
+        private Geolocator _geolocator;
+        private DisplayRequest _displayRequest;
+
+        private Geoposition _lastPosition;
+        private double _totalDistanceMeters = 0;
+
+        public MainPage1()
+        {
+            DisplayInformation.AutoRotationPreferences = DisplayOrientations.Landscape;
+            
+            InitializeComponent();
+
+            StartBatteryMonitoring();
+
+            fullscreenmode();
+        }
+
+
+        protected void fullscreenmode()
+        {
+            // Pantalla completa
+            ApplicationView.GetForCurrentView().TryEnterFullScreenMode();
+        }
+
+        private async void OnStartClick(object sender, RoutedEventArgs e)
+        {
+
+            if (_displayRequest == null)
+            {
+                _displayRequest = new DisplayRequest();
+                _displayRequest.RequestActive(); // Mantener pantalla encendida
+            }
+
+            _geolocator = new Geolocator
+            {
+                DesiredAccuracyInMeters = 5,
+                ReportInterval = 1000 // en milisegundos
+            };
+
+            _geolocator.PositionChanged += Geolocator_PositionChanged;
+
+            BtnStart.Visibility = Visibility.Collapsed;
+            SpeedTextBlock.Foreground = new SolidColorBrush(Color.FromArgb(255, 147, 250, 145));
+        }
+
+        private async void Geolocator_PositionChanged(Geolocator sender, PositionChangedEventArgs args)
+        {
+
+            var pos = args.Position;
+
+            if (_lastPosition != null)
+            {
+                var pos1 = _lastPosition.Coordinate.Point.Position;
+                var pos2 = pos.Coordinate.Point.Position;
+
+                double dist = CalcularDistancia(pos1, pos2);
+                _totalDistanceMeters += dist;
+            }
+
+            _lastPosition = pos;
+            // La velocidad viene en metros por segundo
+            double? speed = args.Position.Coordinate.Speed;
+
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                if (speed.HasValue)
+                {
+                    // Convertimos a km/h si querés
+                    double velocidadKmh = speed.Value * 3.6;
+                    int velC = (int)Math.Ceiling(velocidadKmh);
+                    SpeedTextBlock.Text = $"{velC}";
+                }
+                else
+                {
+                    SpeedTextBlock.Text = "0";
+                }
+
+                OdometerTextBlock.Text = $"{_totalDistanceMeters / 1000:F0} km";
+            });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StartBatteryMonitoring()
+        {
+            Battery.AggregateBattery.ReportUpdated += Battery_ReportUpdated;
+            UpdateBatteryStatus();
+        }
+
+        private async void Battery_ReportUpdated(Battery sender, object args)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, UpdateBatteryStatus);
+        }
+
+        private void UpdateBatteryStatus()
+        {
+            var report = Battery.AggregateBattery.GetReport();
+            if (report.RemainingCapacityInMilliwattHours.HasValue && report.FullChargeCapacityInMilliwattHours.HasValue)
+            {
+                var percent = 100.0 * report.RemainingCapacityInMilliwattHours.Value / report.FullChargeCapacityInMilliwattHours.Value;
+                BatteryTextBlock.Text = $"{percent:F0}%";
+            }
+            else
+            {
+                BatteryTextBlock.Text = "N/D%";
+            }
+        }
+
+        private double CalcularDistancia(BasicGeoposition pos1, BasicGeoposition pos2)
+        {
+            const double RadioTierra = 6371000; // en metros
+
+            double lat1 = pos1.Latitude * Math.PI / 180.0;
+            double lon1 = pos1.Longitude * Math.PI / 180.0;
+            double lat2 = pos2.Latitude * Math.PI / 180.0;
+            double lon2 = pos2.Longitude * Math.PI / 180.0;
+
+            double dLat = lat2 - lat1;
+            double dLon = lon2 - lon1;
+
+            double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                       Math.Cos(lat1) * Math.Cos(lat2) *
+                       Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            return RadioTierra * c;
+        }
+    }
+}
